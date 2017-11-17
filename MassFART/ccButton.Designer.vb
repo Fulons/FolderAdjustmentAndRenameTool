c4d58be11533d7a9631f4186c29b90aa496b4eb9
Fulons <fulons.k@gmail.com>
2017-11-17 23:26:36 +0000

    Trying to fix project

--- MassFART/ccButton.Designer.vb
+++ MassFART/ccButton.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class ccButton
-    Inherits System.Windows.Forms.Button
+Partial Class CCButton
+    Inherits System.Windows.Forms.Control
 
     'Control overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
